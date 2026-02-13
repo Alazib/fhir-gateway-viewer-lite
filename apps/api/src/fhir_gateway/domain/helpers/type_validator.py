3cf41f290a66c7b159f459a01c1d1f4bca626295
J.Alberto <alazib.developer@gmail.com>
2026-02-13 18:50:40 +0100

    refactor(domain): add type validator for value objects

---
+++ apps/api/src/fhir_gateway/domain/helpers/type_validator.py
@@ -0,0 +1,39 @@
+from fhir_gateway.domain.errors import DomainValidationError
+
+
+def type_validator(
+    instance: object,
+    attribute_name: str,
+    expected_type: type | tuple[type, ...],
+    optional_error_message: (
+        str | None
+    ) = None,  # All the validations with "expected_type: tuple[type,...]" require an optional_error_message in order to avoid a final
+    # error message such as "...must be a None"
+) -> None:
+    class_name = instance.__class__.__name__
+    value = instance.__getattribute__(attribute_name)
+
+    if not isinstance(value, expected_type):
+
+        type_names_map = {
+            str: "string",
+            int: "number",
+            float: "number",
+            list: "list",
+            dict: "dictionary",
+            tuple: "tuple",
+            bool: "boolean",
+        }
+
+        expected_type_name = type_names_map.get(expected_type)
+
+        error_message = (
+            f"must be a {expected_type_name}"
+            if optional_error_message is None
+            else optional_error_message
+        )
+
+        raise DomainValidationError(
+            f"{class_name}.{attribute_name}",
+            error_message,
+        )
